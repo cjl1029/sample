8e99ae97bd15ac2cd428af7bd2546ed9d9a061be
clippold623 <95463197+clippold623@users.noreply.github.com>
2022-02-10 03:30:29 -0500

    initial commit

---
+++ src/edu/sru/lippoldchris/sample/sample.java
@@ -0,0 +1,19 @@
+package edu.sru.lippoldchris.sample;
+
+public class sample {
+	private int a;
+
+	public int getA() {
+		return a;
+	}
+
+	public void setA(int a) {
+		this.a = a;
+	}
+	
+	public void printA(int a)
+	{
+		System.out.println(a);
+		
+	}
+}
